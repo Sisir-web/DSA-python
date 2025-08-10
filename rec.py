04c4a86314bc7c5cc42d04741e112a2d08766a68
sisir <sisirrai2407@gmail.com>
2025-08-10 22:44:44 +0530

    recurssion basic

---
+++ rec.py
@@ -0,0 +1,34 @@
+#Question 1 print all natural number in reverse and normal way
+# class R:
+#    def func(self,n):
+#       if n==0:
+#          return 
+#       else:
+#           print(n ,end="<-->")
+#           self.func(n-1)
+          
+# r=R()
+# r.func(100)
+
+# #Question 2  write a recursive function to print all the N odd natural numbers also print in reverse order and do for the even too
+# class R:
+#    def func(self,n):
+#       if n==0 :
+#          return
+#       else:
+#          if n%2==0:      #reverse order
+#             print(n)
+         
+#          self.func(n-1)
+# #          # if n%2!=0:          #ascending order
+# #          #      print("odd :",n)
+# #          # else:
+# #          #    return
+# r=R()
+# r.func(10)
+
+
+#Question 3 
+
+
+
