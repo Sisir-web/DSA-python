3ca350d650728bfbb0ad52e0ea7be429a0db2785
sisir <sisirrai2407@gmail.com>
2025-08-11 20:24:43 +0530

    recurrsion factorial

--- rec.py
+++ rec.py
@@ -62,6 +62,16 @@
 # r=R()
 # s=r.func(10)
 # print(s)
+#Find the factorial of a number
+class R:
+   def func(self,n):
+      if n==1:
+         return 1
+      else:
+         return n*self.func(n-1)
+r=R()
+print(r.func(5))
+
   
       
 
