ea65f915b44577c2c94f75fbd40f68abdb779f63
sisir <sisirrai2407@gmail.com>
2025-08-14 22:22:55 +0530

    I am starting BST now from today 14-Aug-2025

---
+++ bst.py
@@ -0,0 +1,79 @@
+# class Node:
+#    def __init__(self,left=None,data=None,right=None):
+#       self.data=data
+#       self.left=left
+#       self.right=right
+
+# class BST:
+#    def __init__(self):
+#       self.root=None
+
+
+#    def empty(self):
+#       return self.root is None
+   
+#    def insert(self,root,data):
+#       if self.root is None:
+#          return Node(data)
+      
+#       if data < root.data:
+#          root.left=self.insert(root.left,data)
+#       elif data > root.data:
+#          root.right=self.insert(root.right,data)
+#       return root
+   
+#    def inorder(self,root):
+#       if root:
+#          self.inorder(root.left)
+#          print(root.data,end=" ")
+#          self.inorder(root.right)
+
+# bs=BST()
+# root=None
+# for value in [8, 3, 10, 1, 6, 14, 4, 7, 13]:
+#    root=bs.insert(root,value)
+# print(bs.empty())
+       
+# print("Inorder Traversal (Sorted):")
+# bs.inorder(bs.root)
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def empty(self):
+        return self.root is None
+
+    def insert(self, root, data):
+        if root is None:  # check for local root
+            return Node(data)
+
+        if data < root.data:
+            root.left = self.insert(root.left, data)
+        elif data > root.data:
+            root.right = self.insert(root.right, data)
+        return root
+
+    def inorder(self, root):
+        if root:
+            self.inorder(root.left)
+            print(root.data, end=" ")
+            self.inorder(root.right)
+
+# Create tree
+bs = BST()
+for value in [8, 3, 10, 1, 6, 14, 4, 7, 13]:
+    bs.root = bs.insert(bs.root, value)  # Assign to bs.root
+
+# Check if empty
+print(bs.empty())  # Should print False
+
+# Print inorder traversal
+print("Inorder Traversal (Sorted):")
+bs.inorder(bs.root)
